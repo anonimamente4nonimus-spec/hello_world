8b9125eac706252ce10e385002d47d191193c153
anonimamente4nonimus-spec <anonimamente4nonimus@gmail.com>
2025-08-27 16:32:00 -0300

    primeira mudança

---
+++ teste.py
@@ -0,0 +1,8 @@
+class boga:
+    def __init__(self, tamanho):
+        self.tamanho = tamanho
+    
+    def medir(self):
+        print(f"o tamanho do boga é {self.tamanho} cm")
+        
+couzinho = boga(15)
